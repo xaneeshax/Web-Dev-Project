05a48657c6a433ed8148b545bd1e24ceadcf850f
xaneeshax <aneesha.sreerama@gmail.com>
2022-12-03 19:29:58 -0500

    Progress made on FSB UI

--- src/songs/index.js
+++ src/songs/index.js
@@ -2,6 +2,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {useEffect, useState} from "react";
 import {createSongsThunk, deleteSongThunk, findAllSongsThunk} from "./songs-thunks";
 import {userLikesSongThunk} from "../likes/likes-thunks";
+import "./index.css";
 
 const Songs = () => {
     const {currentUser} = useSelector((state) => state.users)
@@ -12,7 +13,7 @@ const Songs = () => {
         dispatch(findAllSongsThunk())
     }, [])
     return(
-        <>
+        <div className="page">
             <h1>Songs</h1>
             {
                 currentUser &&
@@ -55,7 +56,62 @@ const Songs = () => {
                     )
                 }
             </ul>
-        </>
+
+            <h1>Browse</h1>
+            <nav class="main-nav">
+                <a class="link is-active" href="#">Overview</a>
+                <a class="link" href="#">Charts</a>
+                <a class="link" href="#">Genres and Moods</a>
+                <a class="link" href="#">New Releases</a>
+                <a class="link" href="#">Discover</a>
+                <a class="link" href="#">More</a>
+            </nav>
+
+            <h2>Featured Lists</h2>
+            <ul class="music-list">
+                <li class="music-list-item">
+                    <img src="https://images.genius.com/6ec7f3034fb593d32f3298f21aa35245.500x500x1.jpg"/>
+                    <h3 class="song-title">How Can I Forget</h3>
+                    <p class="song-creator">MKTO, Ryan Riback</p>
+                </li>
+                <li class="music-list-item">
+                    <img src="https://images.genius.com/6ec7f3034fb593d32f3298f21aa35245.500x500x1.jpg"/>
+                    <h3 class="song-title">How Can I Forget</h3>
+                    <p class="song-creator">MKTO, Ryan Riback</p>
+                </li>
+                <li class="music-list-item">
+                    <img src="https://images.genius.com/6ec7f3034fb593d32f3298f21aa35245.500x500x1.jpg"/>
+                    <h3 class="song-title">How Can I Forget</h3>
+                    <p class="song-creator">MKTO, Ryan Riback</p>
+                </li>
+                <li class="music-list-item">
+                    <img src="https://images.genius.com/6ec7f3034fb593d32f3298f21aa35245.500x500x1.jpg"/>
+                    <h3 class="song-title">How Can I Forget</h3>
+                    <p class="song-creator">MKTO, Ryan Riback</p>
+                </li>
+                {/* <li class="music__list-item"><span class="song__title">Just Go</span><span class="song__creator">The Goes</span></li>
+                <li class="music__list-item"><span class="song__title">Fanboy</span><span class="song__creator">Young Joe</span></li>
+                <li class="music__list-item"><span class="song__title">Fly</span><span class="song__creator">20 Cent</span></li>
+                <li class="music__list-item"><span class="song__title">Jill</span><span class="song__creator">Don't Even</span></li>
+                <li class="music__list-item"><span class="song__title">Home and Country</span><span class="song__creator">Wandering Ants</span></li>
+                <li class="music__list-item"><span class="song__title">Victor Knows</span><span class="song__creator">The Creatives</span></li>
+                <li class="music__list-item"><span class="song__title">Roadtrippin'</span><span class="song__creator">The Wayfarers</span></li>
+                <li class="music__list-item"><span class="song__title">However You Want</span><span class="song__creator">The Shilts</span></li>
+                <li class="music__list-item"><span class="song__title">Just Say Yes</span><span class="song__creator">The Tweeters</span></li>
+                <li class="music__list-item"><span class="song__title">New York</span><span class="song__creator">V2</span></li>
+                <li class="music__list-item"><span class="song__title">Hallowed Grounds</span><span class="song__creator">Paper and Pencil</span></li>
+                <li class="music__list-item"><span class="song__title">Fanboy</span><span class="song__creator">Young Joe</span></li>
+                <li class="music__list-item"><span class="song__title">Roadtrippin'</span><span class="song__creator">The Wayfarers</span></li>
+                <li class="music__list-item"><span class="song__title">Victor Knows</span><span class="song__creator">The Creatives</span></li>
+                <li class="music__list-item"><span class="song__title">Home and Country</span><span class="song__creator">Wandering Ants</span></li>
+                <li class="music__list-item"><span class="song__title">Hallowed Grounds</span><span class="song__creator">Paper and Pencil</span></li>
+                <li class="music__list-item"><span class="song__title">Springtime Forever</span><span class="song__creator">Near and Far</span></li>
+                <li class="music__list-item"><span class="song__title">Love Song</span><span class="song__creator">The Sprints</span></li>
+                <li class="music__list-item"><span class="song__title">Say It Ain't So</span><span class="song__creator">The Three Beards</span></li>
+                <li class="music__list-item"><span class="song__title">She Goes</span><span class="song__creator">The Kilts</span></li> */}
+            </ul>
+
+        </div>
     )
 }
 
--- src/users/index.js
+++ src/users/index.js
@@ -20,6 +20,42 @@ const Users = () => {
                     )
                 }
             </ul>
+
+            <ul class="sidebar-music">
+            <h2 class="sidebar-header">Your Music</h2>
+                <li>Your Daily Mix</li>
+                <li>Songs</li>
+                {/* <div class="roww">
+                    <div class="blockk">Lorem</div>
+                    <div class="blockk">Ipsum</div>
+                    <div class="blockk">Dolor</div>
+                </div> */}
+                <li>Albums</li>
+                <nav>
+                    <a class="link is-active" href="#">
+                        <img 
+                            id="pfp" 
+                            src="https://i.scdn.co/image/ab67616d0000b273ea3ef7697cfd5705b8f47521" 
+                            width="150"
+                        />
+                    </a>
+                    <a class="link is-active" href="#">
+                        <img 
+                            id="pfp" 
+                            src="https://i.scdn.co/image/ab67616d0000b273ea3ef7697cfd5705b8f47521" 
+                            width="150"
+                        />
+                    </a>
+                    <a class="link" href="#">Charts</a>
+                    <a class="link" href="#">Genres and Moods</a>
+                    <a class="link" href="#">New Releases</a>
+                    <a class="link" href="#">Discover</a>
+                    <a class="link" href="#">More</a>
+                </nav>
+                <li>Artists</li>
+                <li>Favorites</li>
+                <li>Recommended for You</li>
+            </ul>
         </>
     )
 }
